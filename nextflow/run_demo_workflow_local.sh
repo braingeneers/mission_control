8c2f9fd3113f27538b66731bc35e68f095972d09
DailyDreaming <lblauvel@ucsc.edu>
2023-12-01 11:00:02 -0800

    Tweaks.

--- nextflow/run_demo_workflow_local.sh
+++ nextflow/run_demo_workflow_local.sh
@@ -3,16 +3,16 @@
 # This script is used to deploy a workflow on kubernetes, it defaults to workflow demo_workflow.nf if none
 # is specified on the command line. Usage example:
 #
-#   ./run_demo_workflow_local.sh demo_workflow.nf
+#   ./run_demo_workflow_local.sh workflows/demo/main.nf
 #
 
-WORKFLOW_NAME=${1:-demo_workflow.nf}
+WORKFLOW_NAME=${1:-demo/main.nf}
 
-docker run --rm -it \
+docker run --rm -t \
   --volume $(pwd)/workflows/:/workflows/ \
-  --volume ~/.aws/credentials:/root/.aws/credentials \
-  --volume ~/.aws/config:/root/.aws/config \
-  --volume ~/.kube/config:/root/.kube/config \
+  --volume ~/.aws/:/root/.aws/ \
+  --volume ~/.kube/:/root/.kube/ \
+  --volume ~/.local/bin/:/usr/local/sbin/ \
   nextflow/nextflow:latest \
   nextflow run /workflows/${WORKFLOW_NAME} \
   --xyz 1970-01-01-e-demo
