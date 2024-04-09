c1e93ba04ec620a65e96beb7723dc3ff741cfa81
DailyDreaming <lblauvel@ucsc.edu>
2024-04-09 15:42:05 -0700

    Update to use bucket/UUID.

--- nextflow/README.md
+++ nextflow/README.md
@@ -1,101 +1,21 @@
-## Running Nextflow on the [National Research Platform (NRP)](https://portal.nrp-nautilus.io)
+## Nextflow Workflow Service
 
-#### Instructions for running a toy nextflow workflow on the [NRP kubernetes cluster](https://portal.nrp-nautilus.io) (examples use the `braingeneers` namespace; this should be changed to the relevant namespace for your org in both the commands below and the contents of the yml files included).
+Runs nextflow workflows triggered from the "/workflow/start" MQTT topic.
+
+### Running a Workflow
+
+First, create or find a nextflow workflow on github.
+
+If you're writing a nextflow workflow for the first time, a tutorial is here: https://www.nextflow.io/docs/latest/getstarted.html#your-first-script
+
+The github URL for this workflow is the "url" param, for example:
+
+    {"url": "https://github.com/DailyDreaming/k8-nextflow.git"}
+
+Note: Infra has another readme with a more detailed credentials setup (one-time only), as this uses a special service account created especially to run nextflow.
+
+### Instructions for running a toy nextflow workflow on the [NRP kubernetes cluster](https://portal.nrp-nautilus.io) (examples use the `braingeneers` namespace; this should be changed to the relevant namespace for your org in both the commands below and the contents of the yml files included).
 
 ### SETUP (note: this only needs to be done once and is here as a record)
-1. First, create a [PersistentVolumeClaim (PVC)](https://kubernetes.io/docs/concepts/storage/persistent-volumes/):
-
-```commandline
-kubectl apply -f pvc.yml
-```
-
-2. Second, create a service account:
-
-```commandline
-kubectl create serviceaccount nextflow-sa -n braingeneers
-```
-
-3. Create the necessary roles that nextflow needs:
-
-```commandline
-kubectl apply -f resources/configmap_role.yml
-kubectl apply -f resources/jobs_role.yml
-kubectl apply -f resources/pods_role.yml
-kubectl apply -f resources/pods_delete_role.yml
-kubectl apply -f resources/pvc_role.yml
-```
-
-4. Create role bindings to attach those roles to the service account:
-
-```commandline
-kubectl apply -f resources/configmap_rolebinding.yml
-kubectl apply -f resources/jobs_rolebinding.yml
-kubectl apply -f resources/pods_rolebinding.yml
-kubectl apply -f resources/pods_delete_rolebinding.yml
-kubectl apply -f resources/pvc_rolebinding.yml
-```
-
-We now have a service account that should have the permissions necessary to run a nextflow workflow.
-
-5. Create a secret token for the service account (note: this yml produces a token that does not expire):
-
-```commandline
-kubectl apply -f resources/sa-secret.yml
-```
-
-6. Generate a kube config with the above secret token (`GENERATED-SA-TOKEN`).  The token can be generated with:
-
-```commandline
-kubectl get secrets nextflow-sa -o jsonpath='{.data.token}' | base64 --decode
-```
-
-7. You'll also need `CLUSTER-TOKEN`, `SERVER_IP`, and `SERVER_PORT`.  These should already be in your current working kube config file.  Copy and paste them over.
-
-The kube config should look like:
-
-```yaml
-apiVersion: v1
-clusters:
-- cluster:
-    certificate-authority-data: {CLUSTER-TOKEN}
-    server: https://{SERVER_IP}:{SERVER_PORT}
-  name: nautilus
-contexts:
-- context:
-    cluster: nautilus
-    namespace: braingeneers
-    user: nextflow-sa
-  name: nautilus
-current-context: nautilus
-kind: Config
-preferences: {}
-users:
-- name: nextflow-sa
-  user:
-    token: {GENERATED-SA-TOKEN}
-```
 
 ### RUNNING A WORKFLOW
-
-1. If you don't have it installed already, download the latest nextflow binary and make it executable:
-
-```commandline
-wget https://github.com/nextflow-io/nextflow/releases/download/v23.10.1/nextflow
-sudo chmod 1777 nextflow
-```
-
-2. Copy this nextflow config to the current directory:
-
-```commandline
-wget https://raw.githubusercontent.com/DailyDreaming/k8-nextflow/master/nextflow.config
-```
-
-Then run the workflow with the following command:
-
-```commandline
-./nextflow kuberun https://github.com/DailyDreaming/k8-nextflow -v whimvol:/workspace -head-cpus 1 -head-memory 256Mi
-```
-
-NOTE: The toy workflow tests two processes sharing a file, which should test shared access to the same file space [this is the purpose of the [PersistentVolumeClaim (PVC)](https://kubernetes.io/docs/concepts/storage/persistent-volumes/)].
-
-NOTE: [FUSE](https://github.com/s3fs-fuse/s3fs-fuse) requires a plug-in installed on each worker, and so does not seem to be currently feasible without security considerations and cluster-wide changes.
--- nextflow/run.py
+++ nextflow/run.py
@@ -1,51 +1,67 @@
 #!/usr/bin/env python3
 """
-A service that subscribes to the "workflow/start/#" topic and launches workflows in the PRP.
+A service that subscribes to the "workflow/start" topic and launches workflows in the PRP.
 
 Workflows are written in and launch with nextflow (or nextflow's docker image).
+
+This assumes a PVC has been created and is available to mount and share between the worker pods.
 """
-import pytz
 import sys
 import time
 import traceback
 import os
 import shutil
 import subprocess
+import pytz
 
 from uuid import uuid4
 from datetime import datetime
-from braingeneers.utils import smart_open_braingeneers as smart_open
 from braingeneers.iot import messaging
+from typing import Dict, List
 
 
-def launch_nextflow_workflow(params):
-    home_dir = os.path.expanduser('~')
-    if home_dir.endswith('/'):
-        home_dir = home_dir[:-1]
+def get_current_commit_hash(url: str = '') -> str:
+    """Returns the latest 7 char commit hash for a remote github repo."""
+    p = subprocess.run(' '.join(['git', 'ls-remote', url]), shell=True, stdout=-1, stderr=-1)
+    p.check_returncode()
+    return p.stdout.decode()[:7]
+
+
+def launch_nextflow_workflow(params: Dict[str, str]) -> None:
+    github_url = params['url']
 
     nextflow_cmd = [
         'nextflow',
         'kuberun',
-        'https://github.com/DailyDreaming/k8-nextflow',
+        github_url,
         '-v whimvol:/workspace',
         '-head-cpus 1',
-        '-head-memory 256Mi',
-        '-r 1dbdeff',
-        '--xyz zxy'
+        '-head-memory 1024Mi',
+        f'-r {get_current_commit_hash(github_url)}'
     ]
 
-    docker_run_cmd = [
-        'docker',
-        'run',
-        '--rm',
-        f'--volume={home_dir}/.aws/:/root/.aws/',
-        f'--volume={home_dir}/.kube/:/root/.kube/',
-        f'--volume={home_dir}/.local/bin/:/usr/local/sbin/',
-        'quay.io/ucsc_cgl/mqtt-nextflow-s3:0.0'
-    ]
+    for k, v in params.items():
+        if k.strip() and k != 'url':  # skip empty strings; e.g. ''
+            nextflow_cmd.append(f'--{k} {v}')
 
-    p = subprocess.run(' '.join(docker_run_cmd + nextflow_cmd), shell=True)
-    print(p)
+    print(f'Now running: {nextflow_cmd}')
+    current_time = datetime.now(tz=pytz.timezone('UTC')).strftime('%Y-%m-%d_%H-%M-%S')
+    github_url_name = github_url.split('/')[-1][:-len('.git')] if github_url.endswith('.git') else github_url.split('/')[-1]
+    stdout_log = open(f'/workflows/{github_url_name}.{current_time}.stdout', 'w')
+    stderr_log = open(f'/workflows/{github_url_name}.{current_time}.stderr', 'w')
+    p = subprocess.Popen(' '.join(nextflow_cmd), stdout=stdout_log, stderr=stderr_log, shell=True, start_new_session=True)
+    print(f'Workflow has begun: {p}')
+    print('Check workflow logs at: /workflows/')
+
+
+def check_and_move_secrets(src: str, dsts: List[str]):
+    print('Checking and moving credentials files...')
+    for dst in dsts:
+        if not os.path.exists(dst):
+            if not os.path.exists(src):
+                raise RuntimeError(f'{src} does not exist!  Are your secrets mounted?')
+            os.makedirs(dst[:-len('/credentials')], exist_ok=True)
+            shutil.copyfile(src, dst)
 
 
 def main():
@@ -55,31 +71,30 @@ def main():
 
     Note: All timestamps are written in UTC.
     """
-    print('Checking and moving credentials files...')
-    src = '/secrets/prp-s3-credentials/credentials'
-    dsts = ['/root/.aws/credentials', os.path.expanduser('~/.aws/credentials')]
-    for dst in dsts:
-        if not os.path.exists(dst):
-            if not os.path.exists(src):
-                raise RuntimeError(f'{src} does not exist!  Are your secrets mounted?')
-            os.makedirs(dst[:-len('/credentials')], exist_ok=True)
-            shutil.copyfile(src, dst)
+    check_and_move_secrets(
+        src='/secrets/prp-s3-credentials/credentials',
+        dsts=['/root/.aws/credentials', os.path.expanduser('~/.aws/credentials')]
+    )
+    check_and_move_secrets(
+        src='/secrets/nextflow-service-account/kube-config',
+        dsts=['/root/.kube/config', os.path.expanduser('~/.kube/config')]
+    )
+
+    if not os.path.exists('/workflows/'):
+        os.makedirs('/workflows/', exist_ok=True)
 
     print('Starting the nextflow workflow launching service...')
     mb = messaging.MessageBroker("nextflow-launcher-" + str(uuid4()))
-    queue = mb.subscribe_message(topic="workflow/start/#", callback=messaging.CallableQueue())
+    queue = mb.subscribe_message(topic="workflow/start", callback=messaging.CallableQueue())
     while True:
         try:
             topic, params = queue.get()
-            current_time = datetime.now(tz=pytz.timezone('UTC')).strftime('%Y-%m-%d_%H-%M-%S')
-            if isinstance(params, str):
-                params = {'': params}  # gotta have a key
             print(f'{topic}: {params}')
-            launch_nextflow_workflow(params)
-            time.sleep(1)
-        except Exception as e:  # this is a long-lived logging service; never say die
+            if isinstance(params, dict):
+                launch_nextflow_workflow(params)
+        except Exception as e:  # this is a long-lived service; never say die
             print(f'ERROR: {e}\n{traceback.format_exc()}', file=sys.stderr)
-            time.sleep(1)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
