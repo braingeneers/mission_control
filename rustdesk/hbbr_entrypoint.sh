663a52a78da6dec7c13c488201723b15bb50f598
davidparks21 <davidparks21@gmail.com>
2024-04-11 13:04:48 -0700

    minor change to auto-helpdesk service definition and rustdesk

--- rustdesk/hbbr_entrypoint.sh
+++ rustdesk/hbbr_entrypoint.sh
@@ -2,17 +2,17 @@
 
 # Download database files from S3 if they don't exist
 if ! test -f '/root/db_v2.sqlite3'; then
-  aws --endpoint s3.braingeneers.gi.ucsc.edu s3 cp s3://braingeneers/rustdesk/db_v2.sqlite3 /root/
-  aws --endpoint s3.braingeneers.gi.ucsc.edu s3 cp s3://braingeneers/rustdesk/db_v2.sqlite3-shm /root/
-  aws --endpoint s3.braingeneers.gi.ucsc.edu s3 cp s3://braingeneers/rustdesk/db_v2.sqlite3-wal /root/
+  aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp s3://braingeneers/rustdesk/db_v2.sqlite3 /root/rustdesk-data/
+  aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp s3://braingeneers/rustdesk/db_v2.sqlite3-shm /root/rustdesk-data/
+  aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp s3://braingeneers/rustdesk/db_v2.sqlite3-wal /root/rustdesk-data/
 fi
 
 # Back up database files in the background every hour
 {
   while true; do
-    aws --endpoint s3.braingeneers.gi.ucsc.edu s3 cp /root/db_v2.sqlite3 s3://braingeneers/rustdesk/db_v2.sqlite3
-    aws --endpoint s3.braingeneers.gi.ucsc.edu s3 cp /root/db_v2.sqlite3-shm s3://braingeneers/rustdesk/db_v2.sqlite3-shm
-    aws --endpoint s3.braingeneers.gi.ucsc.edu s3 cp /root/db_v2.sqlite3-wal s3://braingeneers/rustdesk/db_v2.sqlite3-wal
+    aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp /root/rustdesk-data/db_v2.sqlite3 s3://braingeneers/rustdesk/db_v2.sqlite3
+    aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp /root/rustdesk-data/db_v2.sqlite3-shm s3://braingeneers/rustdesk/db_v2.sqlite3-shm
+    aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp /root/rustdesk-data/db_v2.sqlite3-wal s3://braingeneers/rustdesk/db_v2.sqlite3-wal
     sleep 3600
   done
 } &
