b3f8b383e8c1cbc34430c2f5e70096bf4635c2da
Kate Voitiuk <kvoitiuk@ucsc.edu>
2025-06-23 00:31:03 -0700

    deperiments-dash adding print statements

--- experiments-dash/experiment-plotly-app.py
+++ experiments-dash/experiment-plotly-app.py
@@ -585,6 +585,7 @@ def update_value(selected_value):
 
 if __name__ == '__main__':
     #app.run(debug=True, port=8051)
+    print("Running the app with SSL context")
     app.run(
         host='0.0.0.0',
         port=8055,
