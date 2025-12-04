641ea98abb8cf57240cfdb18486732f66cce446a
davidparks21 <davidparks21@gmail.com>
2025-12-03 17:35:14 -0800

    Added service cron-braingeneers-jwt-token-refresh

---
+++ cron-braingeneers-jwt-token-refresh/refresh-braingeneers-jwt-service-account-token.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# This script will update a service-account token in our kubernetes namespace daily.
+#
+
+set -euo pipefail
+
+while true:
+  # Get a new JWT service account token (this will only work without authentication from within the braingeneers network)
+  curl -fsS https://service-accounts.braingeneers.gi.ucsc.edu/generate_token > /config.json
+
+  # Update the JWT token in the kubernetes secrets in braingeneers namespace
+  kubectl -n braingeneers delete secret braingeneers-jwt-service-account-token --ignore-not-found
+  kubectl -n braingeneers create secret generic braingeneers-jwt-service-account-token --from-file=config.json
+
+  # Sleep for 1 day
+  sleep 86400
