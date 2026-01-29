2099ab47d7563bacd7992b67e67e188d76ca8f0f
davidparks21 <davidparks21@gmail.com>
2026-01-28 17:51:54 -0800

    upgrade rustdesk to 1.1.15

--- rustdesk/hbbr_entrypoint.sh
+++ rustdesk/hbbr_entrypoint.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
+DB_DIR="/root/rustdesk-data"
+DB_FILE="${DB_DIR}/db_v2.sqlite3"
+DB_SHM="${DB_DIR}/db_v2.sqlite3-shm"
+DB_WAL="${DB_DIR}/db_v2.sqlite3-wal"
+S3_BASE="s3://braingeneers/rustdesk"
+S3_ENDPOINT="https://s3.braingeneers.gi.ucsc.edu"
+
+mkdir -p "${DB_DIR}"
+
 # Download database files from S3 if they don't exist
-if ! test -f '/root/db_v2.sqlite3'; then
-  aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp s3://braingeneers/rustdesk/db_v2.sqlite3 /root/rustdesk-data/
-  aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp s3://braingeneers/rustdesk/db_v2.sqlite3-shm /root/rustdesk-data/
-  aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp s3://braingeneers/rustdesk/db_v2.sqlite3-wal /root/rustdesk-data/
+if ! test -f "${DB_FILE}"; then
+  aws --endpoint "${S3_ENDPOINT}" s3 cp "${S3_BASE}/db_v2.sqlite3" "${DB_FILE}"
+  aws --endpoint "${S3_ENDPOINT}" s3 cp "${S3_BASE}/db_v2.sqlite3-shm" "${DB_SHM}"
+  aws --endpoint "${S3_ENDPOINT}" s3 cp "${S3_BASE}/db_v2.sqlite3-wal" "${DB_WAL}"
 fi
 
 # Back up database files in the background every hour
 {
   while true; do
-    aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp /root/rustdesk-data/db_v2.sqlite3 s3://braingeneers/rustdesk/db_v2.sqlite3
-    aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp /root/rustdesk-data/db_v2.sqlite3-shm s3://braingeneers/rustdesk/db_v2.sqlite3-shm
-    aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp /root/rustdesk-data/db_v2.sqlite3-wal s3://braingeneers/rustdesk/db_v2.sqlite3-wal
+    aws --endpoint "${S3_ENDPOINT}" s3 cp "${DB_FILE}" "${S3_BASE}/db_v2.sqlite3"
+    aws --endpoint "${S3_ENDPOINT}" s3 cp "${DB_SHM}" "${S3_BASE}/db_v2.sqlite3-shm"
+    aws --endpoint "${S3_ENDPOINT}" s3 cp "${DB_WAL}" "${S3_BASE}/db_v2.sqlite3-wal"
     sleep 3600
   done
 } &
 
-# Run hbbr (relay server) in foreground
-hbbr
+# Run hbbr and hbbs in the same container
+hbbr &
+hbbr_pid=$!
+
+hbbs -r rustdesk.example.com:21117 &
+hbbs_pid=$!
+
+trap 'kill ${hbbr_pid} ${hbbs_pid}; wait ${hbbr_pid} ${hbbs_pid}' TERM INT
+wait -n ${hbbr_pid} ${hbbs_pid}
